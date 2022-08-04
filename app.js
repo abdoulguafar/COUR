4ede79324fcbe5626c56622de3dc40cede642827
abdoulguafar <110603070+abdoulguafar@users.noreply.github.com>
2022-08-04 15:07:08 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,30 @@
+
+  
+  /*var nom = prompt("entrer le nom de l utilisateur: "," ");
+  var confirmnom = prompt("confirmer le nom de l utilisateur: "," ");
+      if(nom === confirmnom){
+        alert('nom de l utilisateur');
+    }else{
+        alert('ce n est pas ')
+}*/
+
+
+/*exercice4
+function numberfunction()
+var number = 10;
+   if(number){
+    alert('affiche inferieur a 10');
+   }else{
+    alert('affiche superieur a 10')
+  }*/
+
+/*exercice
+if(x >=60){
+  alert(libre);
+}else if(x >50){
+  alert(mm);
+}else if(x >18){
+  alert(nn);
+}else if(x >17){
+
+}*/
